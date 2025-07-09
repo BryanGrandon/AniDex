9d5fafcd880f2b745422a401c5e22c419aa83d4b
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-07-09 09:54:00 -0400

    styles(web):change slider and defaulCard

--- src/components/DefaultCard.tsx
+++ src/components/DefaultCard.tsx
@@ -7,9 +7,9 @@ interface Default_Card {
 
 const DefaultCard = ({ img, title, episodes }: Default_Card) => {
   return (
-    <a class='block min-h-85 w-58 relative'>
-      <img src={img} alt={`img-${title}`} class='w-full max-h-68 rounded' />
-      <p class='text-gray-200 absolute top-3 right-0  px-2 bg-gray-900 rounded-l-xl '>Episodes: {episodes}</p>
+    <a class='block relative'>
+      <img src={img} alt={`img-${title}`} class='w-full object-cover h-full rounded-lg' />
+      <p class='text-gray-200 absolute top-3 right-0  px-2 bg-gray-900 rounded-l-xl '>Episodes: {episodes ? episodes : 0}</p>
       <h3 class=' bottom-2 left-0 text-gray-650 text-lg p-2'>{title}</h3>
     </a>
   )
--- src/components/layout/Slider.tsx
+++ src/components/layout/Slider.tsx
@@ -4,8 +4,9 @@ import type { Seasons_now } from '../../services/api/interfaces'
 import DefaultCard from '../DefaultCard'
 // Test
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay, EffectCreative, Pagination } from 'swiper/modules'
+import { Autoplay, Pagination } from 'swiper/modules'
 import 'swiper/css'
+import 'swiper/css/grid'
 import 'swiper/css/pagination'
 import 'swiper/css/autoplay'
 
@@ -13,34 +14,41 @@ const Seasons = () => {
   const $season_now: Seasons_now = useStore(seasons_now)
 
   return (
-    <article>
-      <h1>Seasons sss </h1>
+    <article class='p-4'>
+      <h2 class='font-basicaline text-2xl py-5'>
+        Seasons Now: <span class='capitalize text-primary'>{$season_now.data[0].season}</span>
+      </h2>
       <Swiper
         spaceBetween={20}
         slidesPerView={1}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 3,
-          },
-          1024: {
-            slidesPerView: 4,
-          },
-          1280: {
-            slidesPerView: 5,
-          },
-          1600: {
-            slidesPerView: 6,
-          },
-        }}
         loop={true}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
         pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
+        scrollbar={true}
         modules={[Pagination, Autoplay]}
-        className='test  w-full h-100'
+        className=' '
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 4,
+          },
+          1024: {
+            slidesPerView: 5,
+          },
+          1280: {
+            slidesPerView: 6,
+          },
+          1606: {
+            slidesPerView: 7,
+          },
+        }}
       >
         {$season_now.data.map((season) => (
           <SwiperSlide>
