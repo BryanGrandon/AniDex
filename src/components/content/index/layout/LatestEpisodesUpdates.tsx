94d939e09f6ddb0dbb932223d88b9b54472e7b59
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-07-10 18:44:06 -0400

    feat(web):add CardLEU and chenge styles folder

--- src/components/content/index/layout/LatestEpisodesUpdates.tsx
+++ src/components/content/index/layout/LatestEpisodesUpdates.tsx
@@ -1,6 +1,12 @@
 import { useStore } from '@nanostores/preact'
 import { latest_Episodes_Updates } from '../../../../services/storage/general-store'
 import type { LEU } from '../../../../services/api/interfaces/Latest_Episodes_Updates'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Autoplay, Mousewheel } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/autoplay'
+import CardLEU from '../ui/CardLEU'
 
 const LatestEpisodesUpdates = () => {
   const $leu: LEU = useStore(latest_Episodes_Updates)
@@ -9,14 +15,50 @@ const LatestEpisodesUpdates = () => {
     <article>
       <h2>Latest Episodes Updates</h2>
 
-      <article className='flex flex-wrap gap-4'>
-        {$leu.data.map((data) => (
-          <div className='w-40 test h-60 relative overflow-hidden'>
-            <h2 className='overflow-hidden text-ellipsis whitespace-nowrap'>{data.entry.title}</h2>
-            <img src={data.entry.images.webp.large_image_url} alt='' className='object-contain h-40 w-full object-top' />
-            <p>{data.episodes[0].title}</p>
-          </div>
-        ))}
+      <article className=''>
+        <Swiper
+          style={{
+            '--swiper-pagination-color': 'var(--primary)',
+            '--swiper-pagination-right': '8px',
+            '--swiper-pagination-bottom': '-5px',
+            '--swiper-pagination-bullet-inactive-opacity': ' 0.3',
+            '--swiper-pagination-bullet-horizontal-gap': ' 5px',
+          }}
+          spaceBetween={20}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{ delay: 2000, disableOnInteraction: false, pauseOnMouseEnter: true }}
+          pagination={{ clickable: true }}
+          scrollbar={true}
+          modules={[Pagination, Autoplay]}
+          className=''
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+            },
+            640: {
+              slidesPerView: 4,
+            },
+            768: {
+              slidesPerView: 5,
+            },
+            1024: {
+              slidesPerView: 5,
+            },
+            1280: {
+              slidesPerView: 6,
+            },
+            1606: {
+              slidesPerView: 7,
+            },
+          }}
+        >
+          {$leu.data.map((data) => (
+            <SwiperSlide>
+              <CardLEU id={data.entry.mal_id} image={data.entry.images.webp.large_image_url} title={data.entry.title} episodes={data.episodes[0].mal_id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </article>
     </article>
   )
