c8affdb22f9f090bbc0e7cceb8fe1efc929662c0
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-07-18 19:26:14 -0400

    style(web):change TopAiringAnime

--- src/components/content/index/TopAiringAnime.tsx
+++ src/components/content/index/TopAiringAnime.tsx
@@ -4,14 +4,15 @@ import type { TAA } from '../../../services/api/interfaces/top_airing_anime'
 
 const TopAiringAnime = () => {
   const $taa: TAA = useStore(top_airing_anime)
+  // Max 7
   return (
-    <section class=''>
-      {$taa.data.map((data) => (
-        <section>
-          <img src={data.images.webp.small_image_url} alt='' />
-          <section>
+    <section className='flex flex-col gap-2'>
+      {$taa?.data?.map((data) => (
+        <section className='flex test gap-4 px-4'>
+          <p>1</p>
+          <img src={data.images.webp.large_image_url} alt='' className='h-20 w-15 rounded object-cover' />
+          <section className=''>
             <p>{data.title}</p>
-            <p>{data.rank}</p>
           </section>
         </section>
       ))}
