44f79a5ffcb4cfccdd57cc5896558d4022f457ff
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-07-28 16:15:37 -0400

    refactor(web): add Top Publishing Manga Ranked by Score in main

--- src/services/api/getDataApi.ts
+++ src/services/api/getDataApi.ts
@@ -1,4 +1,12 @@
-import { URL_CURRENT_SEASON, URL_LATEST_EPISODES_UPDATES, URL_MOST_POPULAR_ANIME, URL_MOST_POPULAR_MANGA, URL_TOP_AIRING_ANIME, URL_TOP_UPCOMING_ANIME } from '../../utils/constants/urls-api'
+import {
+  URL_CURRENT_SEASON,
+  URL_LATEST_EPISODES_UPDATES,
+  URL_MOST_POPULAR_ANIME,
+  URL_MOST_POPULAR_MANGA,
+  URL_PUBLISHING_MANGA,
+  URL_TOP_AIRING_ANIME,
+  URL_TOP_UPCOMING_ANIME,
+} from '../../utils/constants/urls-api'
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms))
 
@@ -23,6 +31,7 @@ const fetchWithDelay = async (urls: string[]) => {
     else if (url == URL_CURRENT_SEASON) allData.getCS = data
     else if (url == URL_LATEST_EPISODES_UPDATES) allData.getLEU = data
     else if (url == URL_TOP_UPCOMING_ANIME) allData.getTUA = data
+    else if (url == URL_PUBLISHING_MANGA) allData.getPM = data
 
     await delay(500)
   }
--- src/utils/storage/data-index.ts
+++ src/utils/storage/data-index.ts
@@ -1,17 +1,26 @@
 import { atom } from 'nanostores'
-import { URL_CURRENT_SEASON, URL_LATEST_EPISODES_UPDATES, URL_MOST_POPULAR_ANIME, URL_MOST_POPULAR_MANGA, URL_TOP_AIRING_ANIME, URL_TOP_UPCOMING_ANIME } from '../../utils/constants/urls-api'
+import {
+  URL_CURRENT_SEASON,
+  URL_LATEST_EPISODES_UPDATES,
+  URL_MOST_POPULAR_ANIME,
+  URL_MOST_POPULAR_MANGA,
+  URL_PUBLISHING_MANGA,
+  URL_TOP_AIRING_ANIME,
+  URL_TOP_UPCOMING_ANIME,
+} from '../../utils/constants/urls-api'
 import fetchWithDelay from '../../services/api/getDataApi'
 
-const urls = [URL_MOST_POPULAR_ANIME, URL_MOST_POPULAR_MANGA, URL_TOP_AIRING_ANIME, URL_CURRENT_SEASON, URL_LATEST_EPISODES_UPDATES, URL_TOP_UPCOMING_ANIME]
+const urls = [URL_MOST_POPULAR_ANIME, URL_MOST_POPULAR_MANGA, URL_TOP_AIRING_ANIME, URL_CURRENT_SEASON, URL_LATEST_EPISODES_UPDATES, URL_TOP_UPCOMING_ANIME, URL_PUBLISHING_MANGA]
 const allData = await fetchWithDelay(urls)
 
-export const dataCS = allData?.getCS ? allData.getCS : []
+export const dataCS = allData?.getCS ? allData.getCS : [] // Current Season
 
 // Main
-export const dataLEU = allData?.getLEU ? allData.getLEU : []
-export const dataTUA = allData?.getTUA ? allData.getTUA : []
+export const dataLEU = allData?.getLEU ? allData.getLEU : [] // Latest Episodes Updates
+export const dataTUA = allData?.getTUA ? allData.getTUA : [] // Top Upcoming Anime
+export const dataPM = allData?.getPM ? allData.getPM : [] // Publishing Manga
 
 // aside
-export const dataMPA = atom(allData?.getMPA ? allData.getMPA : [])
-export const dataMPM = atom(allData?.getMPM ? allData.getMPM : [])
-export const dataTAA = atom(allData?.getTAA ? allData.getTAA : [])
+export const dataMPA = atom(allData?.getMPA ? allData.getMPA : []) // Most Popular Anime
+export const dataMPM = atom(allData?.getMPM ? allData.getMPM : []) // Most Popular Manga
+export const dataTAA = atom(allData?.getTAA ? allData.getTAA : []) // Top Airing Anime
