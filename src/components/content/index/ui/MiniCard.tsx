be975ff8d95300f5454a29ab6b7b9b0da56bf0b2
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-08-01 15:04:28 -0400

    refactor(web):add info in data.json and change components

--- src/components/content/index/ui/MiniCard.tsx
+++ src/components/content/index/ui/MiniCard.tsx
@@ -7,7 +7,7 @@ type mini_card = {
 
 const MiniCard = ({ image, title, text, highlight }: mini_card) => {
   return (
-    <section class='flex gap-2 rounded-lg bg-black relative overflow-hidden'>
+    <section class='flex gap-2 rounded-lg  bg-dark relative overflow-hidden'>
       <img src={image} alt='' class='h-21 w-15 object-cover' />
       <section class='flex flex-col justify-between p-1 pr-4 w-full'>
         <p>{title}</p>
--- src/components/ui/DefaultCard.tsx
+++ src/components/ui/DefaultCard.tsx
@@ -7,12 +7,12 @@ type Default_Card = {
 }
 const DefaultCard = ({ image, title, highlightText, highlightClass, id }: Default_Card) => {
   return (
-    <section className='relative min-h-full self-baseline'>
-      <img src={image} alt={id + '-img'} className='object-contain rounded-lg' />
+    <section className='relative self-baseline w-full flex flex-col justify-between'>
+      <img src={image} alt={id + '-img'} className='object-cover rounded-lg ' />
       <abbr title={title} className='no-underline'>
         <h3 className='overflow-hidden text-ellipsis whitespace-nowrap cursor-pointer p-2'>{title}</h3>
       </abbr>
-      <p class={highlightClass}>{highlightText}</p>
+      <p class={`bg-dark-transparent absolute shadow shadow-gray-950  ${highlightClass}`}>{highlightText}</p>
     </section>
   )
 }
