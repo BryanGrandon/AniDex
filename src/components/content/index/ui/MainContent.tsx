f2cf4415660c840c43f515c05194307bd7ddb6b9
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-08-01 15:28:06 -0400

    feat(web):add MainContent to avoid repeating code

---
+++ src/components/content/index/ui/MainContent.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from 'preact'
+
+type Props = {
+  title: string
+  description: string
+  children?: JSX.Element
+}
+
+const MainContent = ({ title, description, children }: Props) => {
+  return (
+    <article className='flex flex-col gap-4 p-4'>
+      <section>
+        <h2 className='font-basicaline text-3xl'>{title}</h2>
+        <p className='prose-sm text-gray-400'>{description}</p>
+      </section>
+      <article className='swiper swiperMC w-full'>
+        <section className='swiper-wrapper' id='my-slider'>
+          {children}
+        </section>
+      </article>
+    </article>
+  )
+}
+
+export default MainContent
