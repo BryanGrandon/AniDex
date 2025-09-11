cb6873dd57a6b053282f45cebc64c932bb0b82e4
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-09-11 19:59:05 -0300

    feat(web):add SelectSearchApproach

---
+++ src/components/search/ui/SelectSearchApproach.tsx
@@ -0,0 +1,23 @@
+import useSearch from '../../../utils/hooks/useSearch'
+
+const SelectSearchApproach = () => {
+  const { assignSearchApproach } = useSearch()
+  const variables = ['anime', 'manga', 'season']
+
+  return (
+    <article className='w-full grid grid-cols-3'>
+      {variables.map((el) => (
+        <button
+          className={`border rounded shadow shadow-gray-800 text-2xl font-basicaline capitalize cursor-pointer py-1 hover:scale:105 font-medium ${
+            assignSearchApproach.get == el ? 'bg-primary text-black' : 'bg-dark-transparent'
+          }`}
+          onClick={() => assignSearchApproach.set(el)}
+        >
+          {el}
+        </button>
+      ))}
+    </article>
+  )
+}
+
+export default SelectSearchApproach
