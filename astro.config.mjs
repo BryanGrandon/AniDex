33823cd9bb759353a43bfbc02693f34f96cad75d
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-09-29 09:59:01 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwindcss from '@tailwindcss/vite'
-
 import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://BryanGrandon.github.io',
+  base: '/AniDex',
+
   vite: {
     plugins: [tailwindcss()],
   },
--- src/components/core/ui/FloatingPanel.tsx
+++ src/components/core/ui/FloatingPanel.tsx
@@ -15,10 +15,10 @@ const FloatingPanel = () => {
 
   return (
     <div className='fixed bottom-5 right-5 z-10 flex flex-col gap-3'>
-      <a href={'/'} className='rounded-full p-2 bg-primary text-black cursor-pointer shadow shadow-black'>
+      <a href={'AniDex/'} className='rounded-full p-2 bg-primary text-black cursor-pointer shadow shadow-black'>
         <HomeIcon />
       </a>
-      <a href={'/search'} className='rounded-full p-2 bg-primary text-black cursor-pointer shadow shadow-black flex items-center justify-center'>
+      <a href={'AniDex/search'} className='rounded-full p-2 bg-primary text-black cursor-pointer shadow shadow-black flex items-center justify-center'>
         <SearchIcon />
       </a>
       <button className='z-10 rounded-full p-2 bg-primary text-black cursor-pointer shadow shadow-black hidden-button button-top' onClick={handlerClickMoveTop}>
